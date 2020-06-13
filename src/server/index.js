f18abe5ff9c1bf46e8508a74789c8a97a4ab8da7
Pallavi Nagarkar <66208146+PallaviBN@users.noreply.github.com>
2020-06-14 03:51:37 +0530

    Added final things

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,9 @@ var AYLIENTextAPI = require('aylien_textapi');
 // set aylien API credentials
 var textApi = new AYLIENTextAPI({
   application_id: process.env.API_ID,
+  // application_id: "68837b99",
   application_key: process.env.API_KEY
+  // application_key: "0c18469543844e03b477be1a884e87bc"
 });
 console.log(`Your API key is ${process.env.API_KEY}`);
 
@@ -17,12 +19,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('dist'));
-
-console.log(__dirname);
+app.use(express.static("dist"));
 
 app.post("/api", function(req, res) {
-  const { text } = req.body;
+  const text  = req.body.formText;
   console.log("Request to '/api' endpoint is,", text);
   textApi.sentiment({ text }, (error, response, remaining) => {
     console.log("Aylien Callback", response, remaining);
